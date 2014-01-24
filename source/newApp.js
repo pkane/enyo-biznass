4af0cbe5207f11b2fd8768805d3fb7ca26123234
Paul Kane <pkane@gannett.com>
2014-01-24 14:32:53 -0500

    Attempting to create a new 'newsEntry' control for each story with only the headline, and thereafter the date and body copy for the first five stories. Having issues with the new definition running within the for loop.

--- source/newApp.js
+++ source/newApp.js
@@ -5,30 +5,51 @@ enyo.kind({
   fit: true,
   components: [
     {kind: "WebService", name:"yql", url: "http://api-internal.usatoday.com/MobileServices/MArticleService.svc/mcontent/v1/fronts/tech_tablet", callbackName: "callback"},
-    {kind: "onyx.TextArea", fit:true, classes:"webservice-sample-source", onload: "processRequest", components: [
+    {kind: "newsBody", fit:true, name: "mainBody", classes:"", onload: "processRequest", components: [
 
     ]}
   ],
   processRequest: function (inSender) {
     var request = new enyo.Ajax({url: this.$.yql.url });
+    this.contents = new Array();
     request.response(this, function(inSender, inData) {
-      this.items = inData.modules;
-      // this.content = this.items.indexOf('content');
-      console.log(this.items);
-      for (var i = 0; i <= this.items.length; i++) {
-        console.log(i);
-        if (this.items[i].type == "text") {
-          var newEntry = new onyx.sample.TabBarSample();
-          newEntry().renderInto(this.$.textArea.setValue(JSON.stringify(this.items[i])))
+      this.items = inData.modules.Items.content;      
+      if (this.items) {      
+        for (var i = 0; i <= 5; i++) {
+          this.contents[i] = this.items[i].metaData;                                         
         };
-      };
-      this.$.textArea.setValue(JSON.stringify(this.items, null, 2));
+      } 
+      for (i=0; i<=this.contents.length; i++) {
+        console.log(this.contents[i]);
+        new newsEntry().renderInto(this.$.mainBody);
+        // newEntry.$.headline.setContent(this.contents[i].headline);
+        // // newEntry.$.date.setContent(this.contents[i].dates.published); 
+        // // newEntry.$.bodycopy.setContent(this.contents[i].storyabstract);
+        // newEntry.renderInto(this.$.mainBody); 
+      }
+      // this.$.textArea.setValue(this.contents);      
+      // this.$.textArea.setValue(JSON.stringify(this.items, null, 2));
     });
     request.go();
   }
 });
 
 enyo.kind({
+  name: "newsEntry",
   kind: "Control",
-  classes: "onyx onyx-sample"
-})
+  classes: "onyx onyx-sample",
+  components: [
+    {tag: "h4", name: "headline"},
+    {tag: "span", name: "date"},
+    {tag: "p", name: "bodycopy"}
+  ]
+});
+
+enyo.kind({
+  name: "newsBody",
+  kind: "Control",
+  classes: "onyx onyx-sample",
+  components: [
+    {tag: "div", name: "copy"}
+  ]
+});     
